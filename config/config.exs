cfe298b23d0345d2bdea9fcd3c37cae389a685a3
Maples7 <maples7@163.com>
2019-05-29 17:34:16 +0800

    fix: typo config matadata -> metadata

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,6 @@ use Mix.Config
 #     import_config "#{Mix.env()}.exs"
 config :logger, :console,
   format: {ExJsonLogger, :format},
-  matadata: :all,
+  metadata: :all,
   # `colors` could be disabled in production
   colors: [enabled: false]
